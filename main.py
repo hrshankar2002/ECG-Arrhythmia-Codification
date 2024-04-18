261b93456765dbfebe4c864a8f9ce43c9af228d5
hrshankar2002 <hrshankar2002@gmail.com>
2024-04-18 08:15:16 +0530

    dataset revision

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-import copy
-
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
 import torch
-import torch.nn.functional as F
-from matplotlib import rc
 from mlxtend.plotting import plot_confusion_matrix
 from pylab import rcParams
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from torch import nn, optim
-from torchmetrics import ConfusionMatrix
+from torch import nn
 from tqdm.auto import tqdm
 
-from lib.arff2pandas import arff2pandas as a2p
 from src.data.make_dataset import create_dataset
 from src.model.model import Decoder, Encoder
 from src.model.predict_model import predict, predict_new
 from src.visualization.visualize import plot_prediction
 
-classes = ['N', 'L', 'R', 'V', 'P']
+classes = ["N", "L", "R"]
 
 CLASS_N = 0
 CLASS_L = 1
 CLASS_R = 2
-CLASS_V = 3
-CLASS_P = 4
 
 N_MODEL_PATH = "checkpoints/chkpt_v2/n_model.pth"
 L_MODEL_PATH = "checkpoints/chkpt_v2/l_model.pth"
 R_MODEL_PATH = "checkpoints/chkpt_v2/r_model.pth"
-V_MODEL_PATH = "checkpoints/chkpt_v2/v_model.pth"
-P_MODEL_PATH = "checkpoints/chkpt_v2/p_model.pth"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class RecurrentAutoencoder(nn.Module):
     def __init__(self, seq_len, n_features, embedding_dim=64):
@@ -48,23 +36,19 @@ class RecurrentAutoencoder(nn.Module):
         x = self.decoder(x)
         return x
 
-df = pd.read_csv('data_revisions/revision_3/data_v3.csv')
 
-input_df = df.drop(labels='target', axis=1)
+df = pd.read_csv("notebooks/t.csv")
+
+input_df = df.drop(labels="target", axis=1)
 input_dataset, seq_len, n_features = create_dataset(input_df)
 
 n_model = RecurrentAutoencoder(seq_len, n_features, embedding_dim=120).to(device)
 l_model = RecurrentAutoencoder(seq_len, n_features, embedding_dim=120).to(device)
 r_model = RecurrentAutoencoder(seq_len, n_features, embedding_dim=120).to(device)
-v_model = RecurrentAutoencoder(seq_len, n_features, embedding_dim=120).to(device)
-p_model = RecurrentAutoencoder(seq_len, n_features, embedding_dim=120).to(device)
 
 n_model = torch.load(N_MODEL_PATH, map_location=torch.device("cpu"))
 l_model = torch.load(L_MODEL_PATH, map_location=torch.device("cpu"))
 r_model = torch.load(R_MODEL_PATH, map_location=torch.device("cpu"))
-v_model = torch.load(V_MODEL_PATH, map_location=torch.device("cpu"))
-p_model = torch.load(P_MODEL_PATH, map_location=torch.device("cpu"))
-
 
 
 # accuracy calculation
@@ -73,53 +57,43 @@ def accuracy_fn(y_true, y_pred):
     acc = (correct / len(y_pred)) * 100
     return acc
 
+
 def predn(input_dataset):
     with torch.no_grad():
-        
-        y_pred=[]
+
+        y_pred = []
         for seq_true in tqdm(input_dataset):
-            
+
             loss = []
-    
+
             _, pred_losses_n = predict_new(n_model, [seq_true])
             _, pred_losses_l = predict_new(l_model, [seq_true])
             _, pred_losses_r = predict_new(r_model, [seq_true])
-            _, pred_losses_v = predict_new(v_model, [seq_true])
-            _, pred_losses_p = predict_new(p_model, [seq_true])
 
             loss_n = torch.mean(torch.tensor(pred_losses_n)).item()
             loss_l = torch.mean(torch.tensor(pred_losses_l)).item()
             loss_r = torch.mean(torch.tensor(pred_losses_r)).item()
-            loss_v = torch.mean(torch.tensor(pred_losses_v)).item()
-            loss_p = torch.mean(torch.tensor(pred_losses_p)).item()
-                
+
             loss.append(loss_n)
             loss.append(loss_l)
             loss.append(loss_r)
-            loss.append(loss_v)
-            loss.append(loss_p)
-            
+
             min_index = loss.index(min(loss))
-            
+
             if min_index == 0:
                 y_pred.append(CLASS_N)
             elif min_index == 1:
                 y_pred.append(CLASS_L)
             elif min_index == 2:
                 y_pred.append(CLASS_R)
-            elif min_index == 3:
-                y_pred.append(CLASS_V)
-            else:
-                y_pred.append(CLASS_P)
     return y_pred
+
 
 n_model = n_model.eval()
 l_model = l_model.eval()
 r_model = r_model.eval()
-v_model = v_model.eval()
-p_model = p_model.eval()
 
-y_true = df['target']
+y_true = df["target"]
 y_pred = predn(input_dataset)
 y_true = y_true.values
 y_true = [int(element) for element in y_true]
@@ -128,8 +102,7 @@ acc = accuracy_fn(torch.tensor(y_true), torch.tensor(y_pred))
 print(f"Accuracy: {acc}%")
 
 
-
-# confusion matrix
+# # confusion matrix
 # y_pred = (torch.tensor(y_pred))
 # y_true = (torch.tensor(y_true))
 
@@ -139,14 +112,13 @@ print(f"Accuracy: {acc}%")
 # cm = confusion_matrix(y_true, y_pred)
 
 # fig, ax = plot_confusion_matrix(
-#     conf_mat=cm, 
+#     conf_mat=cm,
 #     class_names=classes,
 #     figsize=(5,5))
 # plt.show()
 
 
-
-# Visualization
+# # Visualization
 # fig, axs = plt.subplots(nrows=5, ncols=6, sharey=True, sharex=True, figsize=(22, 8))
 # for i, data in enumerate(input_dataset[:6]):
 #     plot_prediction(data, n_model, title="Normal Beat", ax=axs[0, i])
@@ -157,8 +129,7 @@ print(f"Accuracy: {acc}%")
 # plt.show()
 
 
-
-# Loss comparison
+# # Loss comparison
 # _, pred_losses_n = predict(n_model, input_dataset[:300])
 # _, pred_losses_l = predict(l_model, input_dataset[:300])
 # _, pred_losses_r = predict(r_model, input_dataset[:300])
